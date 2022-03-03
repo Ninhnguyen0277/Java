6a01e3cdd54a5b75f710996f2cc465ba04e6b347
ninhnguyen <78299736+Ninhnguyen0277@users.noreply.github.com>
2022-03-03 20:00:29 +0700

    Update App08.java

--- Baithuchanhchuong2/src/App08.java
+++ Baithuchanhchuong2/src/App08.java
@@ -1,3 +1,23 @@
+import java.util.Scanner;
+
 public class App08 {
-    
-}
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        int n;
+        int tb=0;
+        System.out.println("nhap so phan tu ");
+        n=sc.nextInt();
+        int A[]= new int[n];
+        System.out.println("nhap cac so nguyen duong ");
+        for(int i=0;i<n;i++){
+            A[i]=sc.nextInt();
+        }
+        for(int i=0;i<n;i++){
+            tb = (tb +A[i]);
+        }
+        tb=tb/n;
+        System.out.println("ket qua la" +tb);
+            sc.close();
+        }
+
+    }
